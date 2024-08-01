23f1711eedc55d5fa1bb58ebc565820ac64d3cbe
Vivekachooz <vivekachooz@gmail.com>
2024-08-01 23:31:19 +0530

    sm6375-common: Add luigi to build guard
    Change-Id: I6545542473a51ada7274511902d2749db6ba2cdd

--- Android.mk
+++ Android.mk
@@ -6,7 +6,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter oscar,$(TARGET_DEVICE)),)
+ifneq ($(filter luigi oscar,$(TARGET_DEVICE)),)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
 include $(CLEAR_VARS)
